d0c9465d9b73382ad36e21f780597f3a0fb17b15
Henry Luengas <henryluengas@gmail.com>
2020-03-04 11:15:59 -0800

    Add files via upload

---
+++ env_variables.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export APP_JDBC_URL="jdbc:mysql://db.labthreesixfive.com/blee96?autoReconnect=true&useSSL=false"
+export APP_JDBC_USER="hluengas"
+export APP_JDBC_PW="WinterTwenty20_365_010845974"
